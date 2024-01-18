598fafcfb5b6c62c92c9dd639e747904a157af6b
DevTinaTi <tinadesk8@gmail.com>
2024-01-18 23:12:06 +0100

    redirect from root to exact root route

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<Main/>} errorElement={<ErrorHandler/>}>
-        <Route path="/" index element={<Navigate to="/login"/>} replace/>
+        <Route index element={<Navigate to="/login"/>} replace/>
           <Route path="registration" element={<Registration/>} />
           <Route path="login" element={<Login/>} />
         <Route
